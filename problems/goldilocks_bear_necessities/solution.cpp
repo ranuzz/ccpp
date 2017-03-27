f78f0e0b49d8279d78da456932df5a62357a1c68
ranu <ranushekhar@gmail.com>
2017-03-27 15:42:48 +0530

    tab to spaces

--- problems/goldilocks_bear_necessities/solution.cpp
+++ problems/goldilocks_bear_necessities/solution.cpp
@@ -24,36 +24,36 @@ using namespace std;
 
 /* it is not required but I'm practicing so... */
 typedef struct bearstuff {
-	int weight;
-	int temp;
+    int weight;
+    int temp;
 }bs;
 
 int girl_wt, girl_temp;
 
 int main()
 {
-	vector<bs> v;
-	cin >> girl_wt >> girl_temp;
-	int w,t;
-	while ( cin >> w >> t) {
-		v.push_back({ w, t});
-	}
-	
-	/* could have been done inline in previous while loop
-	 * doing it here to practice more cpp */
-	auto elem = v.begin();
-	do {
-		elem = find_if(elem,
-						v.end(),
-						[](const bs& r) { return ((r.weight >= girl_wt) && (r.temp <= girl_temp)); });
-	
-		if (elem != v.end()) {
-			cout << (*elem).weight << " " << (*elem).temp << endl;
-			elem++;
-		}
-	} while (elem != v.end());
+    vector<bs> v;
+    cin >> girl_wt >> girl_temp;
+    int w,t;
+    while ( cin >> w >> t) {
+        v.push_back({ w, t});
+    }
+    
+    /* could have been done inline in previous while loop
+     * doing it here to practice more cpp */
+    auto elem = v.begin();
+    do {
+        elem = find_if(elem,
+                        v.end(),
+                        [](const bs& r) { return ((r.weight >= girl_wt) && (r.temp <= girl_temp)); });
+    
+        if (elem != v.end()) {
+            cout << (*elem).weight << " " << (*elem).temp << endl;
+            elem++;
+        }
+    } while (elem != v.end());
 
-	
+    
     return 0;
 }
 
